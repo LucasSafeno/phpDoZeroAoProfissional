57ac8487f6cf4739a06f5e38bf02a9de8561da1b
Lucas Safeno <lugnorio@gmail.com>
2022-06-17 03:23:09 -0300

    PHP do Zero ao Profissional - JavaScript Intermédiario - Aula 2 - Mouse Event

---
+++ 07-js-intermediario/02-eventosMouse/script.js
@@ -0,0 +1,35 @@
+$(function(){
+/*	$('.botao').bind('mousedown', function(){
+			console.log("Apertou o mouse");
+	}); // apertou mouse*/
+
+/*	$('.botao').bind('mouseup', function(){
+			console.log("Soltou o mouse");
+	}); // solta o mouse/click*/
+
+/*		$('.botao').bind('mousemove', function(){
+			console.log("Moveu o mouse");
+	}); // Mover mouse dentro da área*/
+
+/*
+		$('.botao').bind('mouseover', function(){
+			console.log("Mouse está em cima");
+			$(this).addClass('botao_emcima');
+	}); // Mouse executa quando passa por cima
+
+	$('.botao').bind('mouseout', function(){
+		console.log("Tirou o mouse");
+		$(this).removeClass('botao_emcima')
+	}); // quando mouse está em cima e sair executa*/
+
+
+/*	$('.botao').bind('click', function(){
+	 	console.log("Clicou mouse");
+	}); // mouse clica*/
+
+
+	$('.botao').bind('dbmouseup', function(){
+			console.log("double click");
+	}); // double click / click duplo
+
+});
