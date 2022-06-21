dd6ddc1b9b5f80e1442559966ef3762050e4e4b7
Lucas Safeno <lugnorio@gmail.com>
2022-06-21 17:30:32 -0300

    PHP Do Zero ao Profissional - PHP Intermédiario - Aula 21 - Atualizando dados

---
+++ 09-phpIntermediario/pdo/21-atualizandodados.php
@@ -0,0 +1,23 @@
+<?php 
+
+$dsn = "mysql:dbname=blog;host=localhost";
+$dbuser = "root";
+$dbpass = "";
+
+
+
+	try{
+		$pdo = new PDO($dsn, $dbuser, $dbpass);
+
+		// Atualizando dados
+		$sql = "UPDATE usuarios SET email = 'update@update.com' WHERE email = 'lucas@teste.com'  ";
+		$sql = $pdo->query($sql);
+
+		echo "Usuário alterado com sucesso.";
+
+	}catch(PDOExeption $e){
+		echo "Falhou :".$e->getMessage();
+	}
+
+
+?>
