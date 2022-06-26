39e55fefd9094a6760080f412af67c7a1a1efadc
Lucas Safeno <lugnorio@gmail.com>
2022-06-26 20:40:56 -0300

    PHP Do Zero ao Profissional - PHP Intermediario - Verificador HUmano

---
+++ 09-phpIntermediario/11-verificadorHumano/index.php
@@ -0,0 +1,23 @@
+<?php 
+	session_start();
+
+	$n1 = rand(0,10);
+	$n2 = rand(0,10);
+
+	$_SESSION['v'] = $n1 + $n2;
+
+ ?>
+
+<h2>Verificador de Humanos</h2>
+
+<form action="verificador.php" method="post">
+	
+
+	<?php echo $n1 ?> + <?php echo $n2; ?> =
+
+	<input type="number" name="n">
+
+	<input type="submit" value="Verificar">
+
+
+</form>
