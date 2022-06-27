b81a5064d7a19e541bb753a6497c9b08649cdd57
Lucas Safeno <lugnorio@gmail.com>
2022-06-27 14:39:53 -0300

    PHP Do Zero ao Profissional - PHP Avançado - CRUD OO aula 22

---
+++ 12-phpAvançado/03-php-oo/crud-oo/contato.class.php
@@ -0,0 +1,104 @@
+<?php 
+
+class Contato{
+	private $pdo;
+
+	public function __construct(){
+		$this->pdo = new PDO("mysql:dbname=crud_oo;host=localhost","root","root");
+	} // contruct
+
+	// Create
+	public function adicionar($nome, $email){
+
+		if($this->existeEmail($email) == false){
+			$sql = "INSERT INTO contatos SET nome = :nome, email = :email ";
+			$sql = $this->pdo->prepare($sql);
+			$sql->bindValue(":nome", $nome);
+			$sql->bindValue(":email", $email);
+			$sql->execute();
+		}else{	
+			return false;
+		}
+
+	} // adicionar
+
+	// Read
+	public function getNome($email){
+		$sql = "SELECT nome FROM contatos WHERE email = :email";
+		$sql = $this->pdo->prepare($sql);
+		$sql->bindValue(":email", $email);
+		$sql->execute();
+		if($sql->rowCount() > 0){
+			$info = $sql->fetch();
+
+			return $info['nome'];
+		}else{
+			return '';
+		}
+	}// getNome()
+
+
+	public function getAll(){
+		$sql = "SELECT * FROM contatos";
+		$sql = $this->pdo->query($sql);
+
+		if($sql->rowCount() > 0){
+			return $sql->fetchAll();
+		}else{
+			return array();
+		}
+	} //getAll()
+
+
+	// update
+	public function editar($nome, $email){
+		if($this->existeEmail($email)){
+			$sql = "UPDATE contatos SET nome = :nome WHERE email = :email";
+			$sql = $this->pdo->prepare($sql);
+			$sql->bindValue(":nome", $nome);
+			$sql->bindValue(":email", $email);
+			$sql->execute();
+
+			return true;
+		}else{
+			return false;
+		}
+	} // editar()
+
+	// Delete
+	public function excluir($email){
+		if($this->existeEmail($email)){
+			$sql = "DELETE FROM contatos WHERE email = :email";
+			$sql = $this->pdo->prepare($sql);
+			$sql->bindValue(":email", $email);
+			$sql->execute();
+
+			return true;
+		}else{
+			return false;
+		}
+	}
+
+
+	private function existeEmail($email){
+
+		$sql = "SELECT * FROM contatos WHERE email = :email";
+		$sql = $this->pdo->prepare($sql);
+		$sql->bindValue(":email", $email);
+		$sql->execute();
+
+		if($sql->rowCount() > 0){
+			return true;
+		}else{
+			return false;
+		}
+
+	} // existeEmail
+
+
+} // Contato
+
+
+
+
+?>
---
+++ 12-phpAvançado/03-php-oo/crud-oo/index.php
@@ -0,0 +1,10 @@
+<?php 
+include "contato.class.php";
+
+
+$contato = new Contato();
+// $contato->adicionar("Fulano", "email@email.com");
+// $nome = $contato-getNome("email@email.com");
+// $ista = $contato->getAll(); print_r($lista);
+// $contato->editar("Lucas Update", "teste@teste.com");
+ ?>
