7c373dab97bf86ce7d63c738cd8949b8a66d2bd6
Lucas Safeno <lugnorio@gmail.com>
2022-06-26 23:18:39 -0300

    PHP Do Zero ao Profissional - PHP Avançado - Lib GD - Redimencionando e cortando imagem

---
+++ 12-phpAvançado/01-libGD/00-redimencionando-cortando-img.php
@@ -0,0 +1,34 @@
+<?php 
+
+	$arquivo = "imagem.png";
+
+	$largura = 200;
+	$altura = 200;
+
+
+	list($largura_original, $altura_original) = getimagesize($arquivo);
+
+	$ratio = $largura_original / $altura_original;
+
+
+	if($largura / $altura > $ratio){
+		$largura = $altura * $ratio;
+	}else{
+		$altura = $largura / $ratio;
+	}
+
+
+	$imagem_final = imagecreatetruecolor($largura, $altura);
+
+	$imagem_original = imagecreatefrompng($arquivo);
+
+	imagecopyresampled($imagem_final, $imagem_original,
+	 0, 0, 0,0,
+	 $largura, $altura,$largura_original, $altura_original);
+
+
+
+
+	imagepng($imagem_final, "mini_imagem.png");
+
+?>
