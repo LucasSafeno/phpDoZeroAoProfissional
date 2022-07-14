8e52c91e8fe2f8600f78f9def4905d1ef6f4da6d
Lucas Safeno <lugnorio@gmail.com>
2022-07-14 16:01:52 -0300

    PHP Do Zero Ao Profissional - JavaScript Avançado - OO - Proiedades

---
+++ 13-javascriptAvancado/02-introOO-jsAvancado/02-1-proriedade/script.js
@@ -0,0 +1,16 @@
+function Animal(){
+    // Propriedades
+    this.raca   =   "Cão";
+    this.nome   =   "Sunny";
+    this.idade  =   "5";
+    this.peso   =   "10";
+}
+
+var sunny = new Animal()
+sunny.raca = "Cachorro"
+sunny.nome = "Sunny"
+
+
+var akira = new Animal()
+akira.raca = "Gato"
+akira.nome = "Akira"
