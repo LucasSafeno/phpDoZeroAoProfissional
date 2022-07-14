7947466a47aaa51efa02d74995115c3bb94c20eb
Lucas Safeno <lugnorio@gmail.com>
2022-07-14 15:52:47 -0300

    PHP Do Zero Ao Profissional - JavaScript Avançado - Temporização

---
+++ 13-javascriptAvancado/01-temporizacao/script.js
@@ -0,0 +1,12 @@
+
+function acao(){
+    document.write("Executou...<br>")
+}
+
+//setInterval(acao,2000)
+//setTimeout(acao, 2000)
+
+// Parar setInterval e setTimeout
+var timer = setInterval(acao,2000)
+// clearInterval(timer)
+//clearTimeout(timer)
