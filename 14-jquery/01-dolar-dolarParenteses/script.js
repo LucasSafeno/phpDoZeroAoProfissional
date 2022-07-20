8a882622afd259195da7698a681357136fc0f02c
Lucas Safeno <lugnorio@gmail.com>
2022-07-20 19:39:05 -0300

    PHP Do Zero Ao Proissional - JQuery - Document Ready

---
+++ 14-jquery/01-dolar-dolarParenteses/script.js
@@ -0,0 +1,2 @@
+//$.acao = Ação geralmente do próprio Jquery
+//$(elemento).acao = ação pode ser Jquery ou não. Ação feita no elemento selecionado
---
+++ 14-jquery/02-documentReady/script.js
@@ -0,0 +1,12 @@
+/*$(document).ready(function(){
+    alert("Funiconando");
+});*/
+
+/*$(function(){
+    alert("Funcionando também")
+});*/
+
+function tudoPronto(){
+    alert("Funcionando chamando função");
+}
+$(document).ready(tudoPronto);
