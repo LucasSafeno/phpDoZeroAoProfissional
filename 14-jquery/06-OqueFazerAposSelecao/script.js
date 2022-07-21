b0362ca0d3b0c30a67ad012bfb32bd9c2bb61bb9
Lucas Safeno <lugnorio@gmail.com>
2022-07-21 08:43:55 -0300

    PHP Do Zero Ao Profissional - jQuery - O que Faze após seleção

---
+++ 14-jquery/06-OqueFazerAposSelecao/script.js
@@ -0,0 +1,7 @@
+$(function (){
+
+    //$('#teste').html("Texto mudado");
+    // $('#teste button') $('#teste').find('button')
+
+    // $('#teste').find('button').addClass('estilo') // adicionar classes
+});
