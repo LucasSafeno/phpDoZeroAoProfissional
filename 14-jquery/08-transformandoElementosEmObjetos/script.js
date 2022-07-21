3dde94bab04cb8de5b6bb48413a995ccc0ac5c1f
Lucas Safeno <lugnorio@gmail.com>
2022-07-21 14:41:28 -0300

    PHP Do Zero Ao Avançado - JQuery - Transformando Elementos em Objetos

---
+++ 14-jquery/08-transformandoElementosEmObjetos/script.js
@@ -0,0 +1,14 @@
+$(function(){
+
+    /*var titulo = $('h1');
+    titulo.html("Titulo novo") */
+
+    /* $(document.getElementById('div')).html("Texto novo 2")
+    var div = $('#div') */
+
+    $('li:eq(1)').html("modificado item 2")
+
+    var terceiroItem = $('li:eq(2)')
+    terceiroItem.html("Variavel item 3")
+
+});
