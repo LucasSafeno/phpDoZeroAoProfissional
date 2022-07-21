1721f55ae760edb20d9a227bd7467e5de189c780
Lucas Safeno <lugnorio@gmail.com>
2022-07-21 08:23:21 -0300

    PHP Do Zero Ao Profissional - jQuery - Manipulando Atributos

---
+++ 14-jquery/05-ManipulandoAtributosDeTags/script.js
@@ -0,0 +1,6 @@
+$(function(){
+    //$('a').attr('href', 'https://www.google.com.br');
+    //$('img').attr('width', '100'); - muda valor do atributo
+    $('img').attr('border'); // retorna o valor do atributo
+
+});
