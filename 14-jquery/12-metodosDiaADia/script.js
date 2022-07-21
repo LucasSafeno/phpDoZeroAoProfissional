fa261a15c6b4205d2afa6f18dea1af3bb224e15c
Lucas Safeno <lugnorio@gmail.com>
2022-07-21 18:53:12 -0300

    PHP Do Zero ao Avançado - JQuery - Métodos dia a dia

---
+++ 14-jquery/12-metodosDiaADia/script.js
@@ -0,0 +1,7 @@
+$(function() {
+
+    // $.trim() -- Remove espaços em branco antes e depois 
+
+    // $.each()  -- Um loop que executa uma ação a cada elemento
+    // $.type() -- verificar o tipo de uma variavel
+});
