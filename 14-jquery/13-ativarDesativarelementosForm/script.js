3fe85487a3a0e688ca395ea07976b8b1aa9d6690
Lucas Safeno <lugnorio@gmail.com>
2022-07-29 14:40:39 -0300

    PHP Do Zero Ao Avanaçado - JQuery

---
+++ 14-jquery/13-ativarDesativarelementosForm/script.js
@@ -0,0 +1,11 @@
+$(function(){
+    
+    $('#nome').attr('disabled', 'disabled')
+    //$('#nome').removeAttr('disabled')
+
+    //$('#salvo').attr('disabled', 'disabled')
+    //$('#nome').removeAttr('disabled')
+
+    $('#salvo').attr('checked', 'checked')
+    //$('#salvo').removeAttr('checked')
+})
