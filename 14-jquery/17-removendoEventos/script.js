70c39c26a9e09d5dbbfb5e0fca1398700a3a4c9b
Lucas Safeno <lugnorio@gmail.com>
2022-07-29 15:06:43 -0300

    PHP Do Zero Ao Avanaçado - JQuery

---
+++ 14-jquery/17-removendoEventos/script.js
@@ -0,0 +1,8 @@
+$(function(){
+    $('button').bind('click', function(){
+        alert('Clicou')
+    })
+    // $('button').unbind('click') - remove Evento click
+     // $('button').off('click') - remove Evento click
+
+})
