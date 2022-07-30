798e6f0d19c56dfd691f4e01c913b6ecf7f49155
Lucas Safeno <lugnorio@gmail.com>
2022-07-30 01:27:07 -0300

    PHP Do Zero Ao profissional - Jquery

---
+++ 14-jquery/19-efeitoFade/script.js
@@ -0,0 +1,13 @@
+$(function(){
+
+    $('.botao').bind('click', function(){
+        //$('.div').fadeTo('slow', 0.4)
+        $('.div').fadeToggle('slow')
+    })
+
+    // $('.div').fadeOut()  desaparecer da tela
+    //$('.div').fadeIn() - reaparecer na tela
+
+    //$('.div').fadeTo('slow',0.5)
+
+})
