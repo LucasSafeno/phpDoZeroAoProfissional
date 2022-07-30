5ada98131e3ada9a05cfbe6fd7819fe1066b6c6a
Lucas Safeno <lugnorio@gmail.com>
2022-07-30 01:20:35 -0300

    PHP Do Zero Ao profissional - Jquery

---
+++ 14-jquery/18-elementosAparecerDesaparecer/script.js
@@ -0,0 +1,11 @@
+$(function(){
+
+    $('.botao').bind('click', function(){
+        $('.div').toggle('slow');
+    })
+    
+})
+
+// $('.div').hide() - esconde - parametros : slow,fast
+// $('.div').show() - mostra
+//$('.div').toggle() - Se o elemento visivel fica invisivel.
