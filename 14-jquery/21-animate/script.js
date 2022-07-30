344344d3580a4f6a4ba388eb428787428788aee3
Lucas Safeno <lugnorio@gmail.com>
2022-07-30 01:45:54 -0300

    PHP Do Zero Ao profissional - Jquery - Animate

---
+++ 14-jquery/21-animate/script.js
@@ -0,0 +1,12 @@
+$(function(){
+    $('.botao').bind('click', function(){
+
+        $('.div').animate({
+            'margin-left':'100px',
+            'margin-top':'20px',
+            'border-radius': '75px',
+        }, 1000)
+
+
+    })// click botao
+}) // ready
